09ca59a6e98dd6b84afc4dba6793ab112eb3cac7
BOBUR <bobur@MacBook-Air-BOBUR.local>
2026-06-08 00:41:48 +0300

    fix allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   preview: {
     port: 4173,
     host: '0.0.0.0',
+    allowedHosts: ['taxipark-pro-production.up.railway.app', 'all'],
   },
   server: {
     port: 3000,
